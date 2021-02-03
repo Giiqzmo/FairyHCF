3e9d0b1eb3291210713ee1cc4dc6a4167d125169
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-04 00:57:05 +0500

    Create ItemManager.php

---
+++ src/vale/hcf/items/ItemManager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace vale\hcf\items;
+
+use pocketmine\item\Item;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class ItemManager{
+
+	public function initItems(){
+		//todo implement registering
+	}
+
+	public function addHCFItem(Player $player, string $id, int $amount = 1){
+		switch ($id){
+			case "PartnerPackage":
+				$partnerpackage = Item::get(Item::ENDER_CHEST, 0, $amount);
+				$partnerpackage->setCustomName("");
+				$partnerpackage->setLore([
+					'',
+					'',
+				]);
+				$partnerpackage->getNamedTag()->setTag(new StringTag("partnerpackage"));
+				$player->getInventory()->addItem($partnerpackage);
+				break;
+		}
+	}
+}
