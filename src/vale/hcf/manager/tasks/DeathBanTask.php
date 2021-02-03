ae54e0d4b174e91a274c195fd5b17ba49635d64b
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-03 23:27:57 +0500

    Create DeathBanTask.php

---
+++ src/vale/hcf/manager/tasks/DeathBanTask.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace vale\hcf\manager\tasks;
+
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+use vale\hcf\HCF;
+use vale\hcf\manager\DataManager;
+use vale\hcf\manager\DeathBanManager;
+use const pocketmine\DATA;
+
+class DeathbanTask extends Task
+{
+
+	/** @var HCF */
+	public HCF $plugin;
+	/** @var Player */
+	public Player $player;
+
+	public function __construct(HCF $plugin)
+	{
+		$this->plugin = $plugin;
+	}
+
+	public function onRun(int $currentTick)
+	{
+		$deathbanned = HCF::getInstance()->getDeathBannedData()->getAll();
+		foreach ($deathbanned as $p => $time) {
+			if (HCF::$deathBannedPlayers->get($p) <= 1) {
+				HCF::$deathBannedPlayers->remove($p);
+				HCF::$deathBannedPlayers->save();
+
+			} else {
+				HCF::$deathBannedPlayers->set($p, $time - 1);
+				HCF::$deathBannedPlayers->save();
+				//todo lives
+			}
+		}
+	}
+}
