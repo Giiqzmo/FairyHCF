cf45e85ef9adf585a336020f474d530411ef9399
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-03 23:27:40 +0500

    Create DeathBanManager.php

---
+++ src/vale/hcf/manager/DeathBanManager.php
@@ -0,0 +1,73 @@
+<?php
+namespace vale\hcf\manager;
+
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+use vale\hcf\HCF;
+
+class DeathBanManager
+{
+
+	public function setDeathBan(Player $player, int $value)
+	{
+		$data = HCF::getInstance()->getDeathBannedData();
+		if (!$data->exists($player->getName())) {
+			$data->set($player->getName(), (int)$value);
+			$data->save();
+		}
+	}
+
+
+	public function removeDeathBan(Player $player)
+	{
+		$data = HCF::getInstance()->getDeathBannedData();
+		if ($data->exists($player->getName())) {
+			$data->remove($player->getName());
+		}
+
+	}
+
+	public function isDeathBanned(Player $player): bool
+	{
+		$data = HCF::getInstance()->getDeathBannedData();
+		if ($data->exists($player->getName())){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+    public function getDeathBan(Player $player)
+	{
+		$data = HCF::getInstance()->getDeathBannedData();
+		if($data->exists($player->getName())){
+			$time =  (int) $data->get($player->getName());
+			return (int) $time;
+		}else{
+			return (int) 0;
+		}
+	}
+
+	public function getLives(Player $player){
+		$lives = HCF::getInstance()->getLivesData();
+            $lives->get($player->getName());
+            return (int)$lives;
+	}
+
+	public function setLives(Player $player, int $lives){
+
+	}
+
+	public function addLives(Player $player, int $lives){
+
+	}
+
+	public function hasLives(Player $player): bool{
+		return true;
+	}
+
+	public function sendLives(Player $sender, Player $recipient, int $value): void{
+	}
+
+}
