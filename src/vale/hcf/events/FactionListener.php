ea0ba4d6c8b00903451300eac9935971094577e0
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-04 09:16:19 -0500

    Update FactionListener.php

--- src/vale/hcf/events/FactionListener.php
+++ src/vale/hcf/events/FactionListener.php
@@ -1,8 +1,14 @@
 <?php
 namespace vale\hcf\events;
-
+use vale\hcf\HCF;
 class FactionListener implements Listener{
 
-public function_construct(){}
-
+  public HCF $plugin;
+  
+public function __construct(HCF $plugin){}
+ $this->plugin = $plugin;
+ $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+}
+   
+ public function FactionChat(){}
 }
