fa10ece338bd2e6e016fdc11b1789bdd6f920a28
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-08 16:36:57 -0500

    Extensive Core Rewrite

--- src/vale/hcf/HCF.php
+++ src/vale/hcf/HCF.php
@@ -7,7 +7,11 @@ use muqsit\invmenu\InvMenuHandler;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use vale\hcf\cmds\{
-    BlacklistCommand, FactionCommand, WarnCommand, SotwCommand
+	FactionCommand, SotwCommand
+};
+
+use vale\hcf\cmds\mod\{
+BlacklistCommand, WarnCommand, CrateBlockCommand
 };
 use vale\hcf\events\{
     CrateListener, PlayerListener
@@ -16,163 +20,133 @@ use vale\hcf\factions\{
    FactionLoader, FactionListener
 };
 use vale\hcf\manager\{
-   DataManager, DeathBanManager, CrateManager
+   DataManager, DeathBanManager, CrateManager, RanksManager
 };
-use vale\hcf\manager\tasks\DeathbanTask;
+use vale\hcf\manager\tasks\{
+	DeathbanTask, BroadcastTask
+};
+use vale\hcf\data\YamlProvider;
 
 class HCF extends PluginBase
 {
-    /** @var Config $warns */
-    public static Config $warns;
-    
-    /** @var Config $lives */
-    public static Config $lives;
-    
-    /** @var Config $deaths */
-    public static Config $deaths;
-    
-    /** @var Config $kills */
-    public static Config $kills;
-    
-    /** @var HCF $instance */
-    public static HCF $instance;
-    
-    /** @var DataManager $dataManager */
-    public static DataManager $dataManager;
-    
-    public static Config $blacklistedPlayers;
-    
-    /** @var Config $deathBannedPlayers */
-    public static Config $deathBannedPlayers;
-    
-    public static DeathBanManager $deathBanManager;
-    
-    public static Config $crateData;
-    
-    public static SQLite3 $factionData;
-    
-    public FactionLoader $factionManager;
-    /** @var string[] $worlds */
-    public array $worlds = ["test", "uh", "ok"];
 
-    public static function getInstance(): HCF
-    {
-        return self::$instance;
-    }
+	/** @var HCF $instance */
+	public static HCF $instance;
 
-    public function onEnable(): void
-    {
-        if (!InvMenuHandler::isRegistered()) {
-            InvMenuHandler::register($this);
-        }
-        @mkdir($this->getDataFolder() . "lives");
-        @mkdir($this->getDataFolder() . "deaths");
-        @mkdir($this->getDataFolder() . "crates");
-        @mkdir($this->getDataFolder() . "warns");
-        @mkdir($this->getDataFolder() . "kills");
-        self::$instance = $this;
-        $this->initDatabase();
+	public static Config $crateData;
+
+	public static SQLite3 $factionData;
+
+	public static FactionLoader $factionManager;
+	/** @var string[] $worlds */
+	public array $worlds = ["test", "uh", "ok"];
+
+	public static function getInstance(): HCF
+	{
+		return self::$instance;
+	}
+
+	public function onEnable(): void
+	{
+		if (!InvMenuHandler::isRegistered()) {
+			InvMenuHandler::register($this);
+		}
+		self::$instance = $this;
+		YamlProvider:: __initiateRegistration();
+		$this->initFactions();
         $this->loadWorlds();
         $this->loadCommands();
         $this->initListeners();
         $this->getScheduler()->scheduleRepeatingTask(new DeathbanTask($this), 20);
     }
 
-    function initDatabase()
-    {
-        self::$factionManager = new FactionLoader($this);
-        self::$lives = new Config($this->getDataFolder() . "lives/" . "lives.yml");
-        self::$deaths = new Config($this->getDataFolder() . "deaths/" . "deaths.yml");
-        self::$kills = new Config($this->getDataFolder() . "kills/" . "kills.yml");
-        self::$warns = new Config($this->getDataFolder() . "warns/" . "warns.yml");
-        self::$crateData = new Config($this->getDataFolder() . "crates/" . "CrateData.yml");
-        self::$deathBannedPlayers = new Config($this->getDataFolder() . "deathbannedplayers.yml");
-        self::$blacklistedPlayers = new Config($this->getDataFolder() . "blacklistedplayers.yml");
-        self::$dataManager = new DataManager($this);
-        self::$deathBanManager = new DeathBanManager();
-    }
+	function initFactions()
+	{
+		self::$factionManager = new FactionLoader($this);
+		new FactionListener($this);
+	}
 
-    function loadWorlds()
-    {
-        $wrldcount = count($this->worlds);
-        for ($i = 0; $i < $wrldcount; $i++) {
-            $this->getServer()->loadLevel($this->worlds[$i]);
-            $this->getServer()->getLogger()->info("Preparing the worlds " . (string)$this->worlds[$i]);
-        }
-    }
+	function loadWorlds()
+	{
+		$wrldcount = count($this->worlds);
+		for ($i = 0; $i < $wrldcount; $i++) {
+			$this->getServer()->loadLevel($this->worlds[$i]);
+			$this->getServer()->getLogger()->info("Preparing the worlds " . (string)$this->worlds[$i]);
+		}
+	}
 
-    function loadCommands()
-    {
-        $map = $this->getServer()->getCommandMap();
-        $map->registerAll("HCF", [
-            new BlacklistCommand($this),
-            new WarnCommand($this),
-            new FactionCommand($this),
+	function loadCommands()
+	{
+		$map = $this->getServer()->getCommandMap();
+		$map->registerAll("HCF", [
+			new BlacklistCommand($this),
+			new WarnCommand($this),
+			new FactionCommand($this),
 			new SotwCommand($this),
-        ]);
+		]);
 
-    }
+	}
 
-    function initListeners()
-    {
-        new PlayerListener($this);
-        new CrateListener($this);
-        new FactionListener($this);
-    }
+	function initListeners()
+	{
+		new PlayerListener($this);
+		new CrateListener($this);
+	}
 
-    public function getWarnData(): Config
-    {
-        return self::$warns;
-    }
+	public function getWarnData(): Config
+	{
+		return YamlProvider::$warns;
+	}
 
-    public function getKillsData(): Config
-    {
-        return self::$kills;
-    }
+	public function getKillsData(): Config
+	{
+		return YamlProvider::$kills;
+	}
 
-    public function getDeathsData(): Config
-    {
-        return self::$deaths;
-    }
+	public function getDeathsData(): Config
+	{
+		return YamlProvider::$deaths;
+	}
 
-    public function getLivesData(): Config
-    {
-        return self::$lives;
-    }
+	public function getLivesData(): Config
+	{
+		return YamlProvider::$lives;
+	}
 
-    public function getDeathBannedData(): Config
-    {
-        return self::$deathBannedPlayers;
-    }
+	public function getDeathBannedData(): Config
+	{
+		return YamlProvider::$deathBannedPlayers;
+	}
 
-    public function getDeathBanManager(): DeathBanManager
-    {
-        return self::$deathBanManager;
-    }
+	public function getDeathBanManager(): DeathBanManager
+	{
+		return YamlProvider::$deathBanManager;
+	}
 
-    public function getDataManager(): DataManager
-    {
-        return self::$dataManager;
-    }
+	public function getDataManager(): DataManager
+	{
+		return YamlProvider::$dataManager;
+	}
 
-    public function getFactionManager(): FactionLoader
-    {
-        return self::$factionManager;
-    }
+	public function getFactionManager(): FactionLoader
+	{
+		return self::$factionManager;
+	}
 
-    public function secondsToTime(int $secs)
-    {
-        $s = $secs % 60;
-        $m = floor(($secs % 3600) / 60);
-        $h = floor(($secs % 86400) / 3600);
-        $d = floor(($secs % 2592000) / 86400);
-        $M = floor($secs / 2592000);
+	public function getFactionData(): SQLite3
+	{
+		return self::$factionData;
+	}
 
-        return "$d days $h hours $m minutes $s seconds";
-    }
 
-    public function getFactionData(): SQLite3
-    {
-        return self::$factionData;
-    }
+	public function secondsToTime(int $secs)
+	{
+		$s = $secs % 60;
+		$m = floor(($secs % 3600) / 60);
+		$h = floor(($secs % 86400) / 3600);
+		$d = floor(($secs % 2592000) / 86400);
+		$M = floor($secs / 2592000);
+
+		return "$d days $h hours $m minutes $s seconds";
+	}
 }
