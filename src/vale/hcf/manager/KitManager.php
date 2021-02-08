956603a28b79a4a542ea99a962e3c7a3b31e4dab
THXC <cwpenguinmaster2005@gmail.com>
2021-02-08 05:33:20 -0800

    Update KitManager.php

--- src/vale/hcf/manager/KitManager.php
+++ src/vale/hcf/manager/KitManager.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types = 1);
-
 namespace vale\hcf\manager;
 
 use vale\hcf\HCF;
@@ -14,31 +12,37 @@ class KitManager {
   
 	private $data = [];
 
-	  public function __construct(HCF $plugin) {
-		  $this->plugin = $plugin;
-	  }
+	public function __construct(HCF $plugin) {
+		$this->plugin = $plugin;
+		if(file_exists($this->getCooldownPath() . DIRECTORY_SEPARATOR . "cooldown.json")){
+            		$this->data = json_decode(file_get_contents($this->getCooldownPath() . DIRECTORY_SEPARATOR . "cooldown.json"), true);
+        	}
+	}
 
-	  public function getKitByName(string $kit) : ?Kit {
-		  return $this->kits[$kit] ?? null;
-	  }
+	public function getKitByName(string $kit) : ?Kit {
+		return $this->kits[$kit] ?? null;
+	}
 
-	  public function getKits(): array {
-	    //todo
-    }
+	public function getKits(): array {
+		return $this->kits;
+    	}
 
-	  public function getCooldown(string $kit, string $player): int{
-	    //todo
-    }
+	public function getCooldown(string $kit, string $player): int{
+		return isset($this->data[$kit][$player]) ? $this->data[$kit][$player] : 0;
+    	}
 
-    public function addToCooldown(string $kit, string $player, int $time): void{
-      //todo
-    }
+    	public function addToCooldown(string $kit, string $player, int $time): void{
+      		$this->data[$kit][$player] = time();
+    	}
 
-    public function removeFromCooldown(string $kit, string $player): void{
-	    //todo
-    }
+    	public function removeFromCooldown(string $kit, string $player): void{
+	    	if(isset($this->data[$kit][$player]))
+        	unset($this->data[$kit][$player]);
+    	}
 
-	  public function getCooldownPath(): string{
-	    return HCF::getInstance()->getDataFolder() . "kit";
-    }
+	public function getCooldownPath(): string{
+	    	return HCF::getInstance()->getDataFolder() . "kit";
+    	}
+	
+	//add a save() function?
 }
