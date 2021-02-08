53b053a1e9b8323e3319954f8afdb3b09614bb37
THXC <cwpenguinmaster2005@gmail.com>
2021-02-08 09:26:14 -0800

    Create BroadcastTask.php

---
+++ src/vale/hcf/manager/tasks/BroadcastTask.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace vale\hcf\manager\tasks;
+
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+use pocketmine\utils\TextFormat as C;
+use vale\hcf\HCF;
+
+class BroadcastTask extends Task
+{
+
+    /** @var HCF */
+    public HCF $plugin;
+    
+    /** @var Player */
+    public Player $player;
+    
+    /** @var Messages */
+    public Array $messages = []
+    
+    public function __construct(HCF $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    public function onRun(int $currentTick)
+    {
+        //shit goes here
+    }
+}
