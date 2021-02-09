e45c9efe5267b779cc861d1d0255b7f382aefbd1
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-09 16:34:17 -0500

    Update ScoreBoardTask.php

--- src/vale/hcf/manager/tasks/ScoreBoardTask.php
+++ src/vale/hcf/manager/tasks/ScoreBoardTask.php
@@ -5,10 +5,12 @@ namespace vale\hcf\manager\tasks;
 use pocketmine\Player;
 use pocketmine\scheduler\Task;
 use vale\hcf\data\YamlProvider;
-use vale\hcf\factions\FactionLoader;
 use vale\hcf\HCF;
-use vale\hcf\manager\ScoreBoardManager;
+use vale\hcf\manager\{
+	SotwManager, ScoreBoardManager
+};
 use pocketmine\utils\TextFormat as TE;
+use vale\hcf\factions\FactionLoader;
 
 class ScoreboardTask extends Task
 {
@@ -30,26 +32,40 @@ class ScoreboardTask extends Task
 		$this->player = $player;
 	}
 
-	public function onRun(int $currentTick): void
-	{
+	public function onRun(Int $currentTick) : void {
 		$player = $this->player;
-		if (!$player->isOnline()) {
+		if(!$player->isOnline()){
 			HCF::getInstance()->getScheduler()->cancelTask($this->getTaskId());
 			return;
 		}
-		$factionManager = new FactionLoader(HCF::getInstance());
-		$this->faction = $factionManager->getPlayerFaction($player->getName());
-		$this->dtr = $factionManager->getFactionDTR($this->faction);
-		$this->homeCoords = $factionManager->getHome($this->faction);
+		$api = new ScoreBoardManager();
+		$facs = new FactionLoader(HCF::getInstance());
+		$this->faction = $facs->getPlayerFaction($player->getName());
+		/** @var array */
+		$scoreboard = [];
+		if($facs->isInFaction($player)){
+			$scoreboard[] = "§6§lTeam§r§7: " . (string)$this->faction;
+		}
+		if(SotwManager::isEnable()){
+			$scoreboard[] = "§r§6§lSOTW§r§7:§r§e " . HCF::getTimeToFullString(SotwManager::getTime());
+		}
 
-		$this->api = new ScoreBoardManager();
-		$api = $this->api;
-		$api->newScoreboard($player, "ScoreBoard", "§r§6Fairy | §r§eInferno Realm");
-		if ($factionManager->isInFaction($player)) {
-			$api->setLine($player, 1, "§r§7-----------------");
-			$api->setLine($player, 2, "§6§lTeam§r§7: " . (string)$this->faction);
-			$api->setLine($player, 3, "§6§lHQ§r§7: " . (string)$this->homeCoords);
-			$api->setLine($player, 4, "§6§lDTR§r§7 §r§c" . $this->checkDtr($player));
+		if(count($scoreboard) >= 1){
+			$scoreboard[] = TE::GRAY."------------------- ";
+			$texting = [TE::GRAY.TE::GRAY."------------------- "];
+			$scoreboard = array_merge($texting, $scoreboard);
+		}else{
+			$api->removePrimary($player);
+			return;
+		}
+		$api->newScoreboard($player, $player->getName(), "§r§6Fairy | §r§eInferno Realm");
+		if($api->getObjectiveName($player) !== null){
+			foreach($scoreboard as $line => $key){
+				$api->remove($player, $scoreboard);
+				$api->newScoreboard($player, $player->getName(), "§r§6Fairy | §r§eInferno Realm");			}
+		}
+		foreach($scoreboard as $line => $key){
+			$api->setLine($player, $line + 1, $key);
 		}
 	}
 
