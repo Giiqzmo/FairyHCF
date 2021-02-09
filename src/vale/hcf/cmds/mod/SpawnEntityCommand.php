8a7729555469979c33f9ae4c9ab457b737408d19
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-08 19:33:10 -0500

    Create SpawnEntityCommand.php

---
+++ src/vale/hcf/cmds/mod/SpawnEntityCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace vale\hcf\cmds\mod;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\entity\Entity;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use vale\hcf\entities\PartnerPackageEntity;
+use vale\hcf\HCF;
+
+class SpawnEntityCommand extends PluginCommand{
+
+	public function __construct(string $name, HCF $owner)
+	{
+		parent::__construct($name, $owner);
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args)
+	{
+		if ($sender instanceof Player) {
+			if (!isset($args[0])) {
+				$sender->sendMessage("/spawnentity <name>");
+				return false;
+			}
+			switch ($args[0]) {
+				case "partner":
+					$bot = new PartnerPackageEntity($sender->getLevel(), Entity::createBaseNBT($sender->asVector3()), $sender);
+
+					$bot->spawnToAll();
+					$bot->setCanSaveWithChunk(true);
+					break;
+			}
+		}
+		return true;
+	}
+
+}
