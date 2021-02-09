b3607b7cea90c2c14bf56899b5b98737a26f3a8b
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-09 16:36:02 -0500

    Update SotwCommand.php

--- src/vale/hcf/cmds/SotwCommand.php
+++ src/vale/hcf/cmds/SotwCommand.php
@@ -2,36 +2,64 @@
 
 namespace vale\hcf\cmds;
 
-use pocketmine\command\ConsoleCommandSender;
-use pocketmine\command\PluginCommand;
-use pocketmine\Player;
-use pocketmine\Server;
+use vale\hcf\manager\SotwManager;
 use vale\hcf\HCF;
 
-class SotwCommand extends PluginCommand
-{
+use pocketmine\command\{CommandSender, PluginCommand};
+use pocketmine\utils\TextFormat as TE;
 
-	public $plugin;
+class SOTWCommand extends PluginCommand {
 
-	public function __construct(HCF $plugin)
-	{
-		$this->plugin = $plugin;
-		parent::__construct("sotw", $plugin);
+	/**
+	 * SOTWCommand Constructor.
+	 */
+	public function __construct(){
+		parent::__construct("sotw", HCF::getInstance());
 	}
 
-	public function execute(\pocketmine\command\CommandSender $sender, string $commandLabel, array $args)
-	{
-		if (!isset($args[0])) {
-			$sender->sendMessage("/sotw <time>");
-			return false;
+	/**
+	 * @param CommandSender $sender
+	 * @param String $label
+	 * @param array $args
+	 * @return void
+	 */
+	public function execute(CommandSender $sender, String $label, Array $args) : void {
+		if(count($args) === 0){
+			$sender->sendMessage("");
+			return;
 		}
-		if(!is_numeric($args[0])){
-			$sender->sendMessage("Second Argument must be of int not instance of string");
-			return false;
+		if(!$sender->isOp()){
+			$sender->sendMessage(TE::RED."§r§7((TIP: §r§cYou must be a operator to use this command §r§7))");
+			return;
 		}
-		if (is_numeric($args[0])) {
-			$sender->sendMessage("Started Sotw for ". HCF::getInstance()->secondsToTime($args[0]));
+		switch($args[0]){
+			case "on":
+				if(!$sender->isOp()){
+					$sender->sendMessage(TE::RED."§r§7((TIP: §r§cYou must be a operator to use this command §r§7))");
+					return;
+				}
+				if(empty($args[1])){
+					$sender->sendMessage("§r§7((TIP:/sotw §r§ctime §r§7[Int: time]");
+					return;
+				}
+				if(SotwManager::isEnable()){
+					$sender->sendMessage(TE::RED."§r§7((TIP: §r§cSotw is Already Running§r§7))");
+					return;
+				}
+				SotwManager::start($args[1]);
+				break;
+			case "off":
+				if(!$sender->isOp()){
+					$sender->sendMessage(TE::RED."§r§7((TIP: §r§cYou must be a operator to use this command §r§7))");
+
+					return;
+				}
+				if(!SotwManager::isEnable()){
+					$sender->sendMessage(TE::RED."§r§7((TIP: §r§cStart of the World has to be on§r§7))");
+					return;
+				}
+				SotwManager::stop();
+				break;
 		}
-		return true;
 	}
 }
