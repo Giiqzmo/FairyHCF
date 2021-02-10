6800aaf7966c13a8cbeae173f199d06b0bcc523b
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-09 21:22:49 -0500

    Update FactionListener.php

--- src/vale/hcf/factions/FactionListener.php
+++ src/vale/hcf/factions/FactionListener.php
@@ -2,21 +2,41 @@
 
 namespace vale\hcf\factions;
 
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
+use pocketmine\Player;
 use vale\hcf\HCF;
 
 class FactionListener implements Listener
 {
 
-    public HCF $plugin;
+	public HCF $plugin;
 
-    public function __construct(HCF $plugin)
-    {
-        $this->plugin = $plugin;
-        $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
-    }
+	public function __construct(HCF $plugin)
+	{
+		$this->plugin = $plugin;
+		$plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+	}
 
-    public function factionChat()
-    {
-    }
+	/**
+	 * @param EntityDamageByEntityEvent $event
+	 */
+	public function friendlyFire(EntityDamageByEntityEvent $event)
+	{
+		/** @var $entity Player */
+		$entity = $event->getEntity();
+		/** @var $damager Player */
+		$damager = $event->getDamager();
+		$mngr = HCF::getInstance()->getFactionManager();
+		if ($entity instanceof Player && $damager instanceof Player) {
+			if ($mngr->getPlayerFaction($entity->getName() === $mngr->getPlayerFaction($damager))) {
+				if (FactionLoader::hasFriendlyFireEnabled($entity) && FactionLoader::hasFriendlyFireEnabled($damager)) {
+					$event->setBaseDamage(0);
+				} else {
+					$event->setCancelled(true);
+					$damager->sendMessage("You cannot attack your faction members");
+				}
+			}
+		}
+	}
 }
