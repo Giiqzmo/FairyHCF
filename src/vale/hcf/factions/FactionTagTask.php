670394d88b69cb0b88aa887988b299b9221c8add
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-12 18:40:07 -0500

    Create FactionTagTask.php

---
+++ src/vale/hcf/factions/FactionTagTask.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace vale\hcf\events;
+
+
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use vale\hcf\factions\FactionLoader;
+use vale\hcf\HCF;
+
+class FactionTagTask extends Task
+{
+
+    /**
+     * FactionTagTask constructor.
+     * @param Player $player
+     * @param mixed|string $facname
+     */
+
+    public $player;
+    public string $facname;
+    public function __construct(Player $player, string $facname){
+    	$this->player =  $player;
+    	$this->facname = $facname;
+    }
+
+    public function onRun(int $currentTick)
+	{
+		$onlinePlayers = Server::getInstance()->getOnlinePlayers();
+		$mngr = new FactionLoader(HCF::getInstance());
+		$faction = $mngr->getPlayerFaction($this->player->getName());
+		$members = $mngr->getAllMembers($faction);
+		foreach ($onlinePlayers as $onlinePlayer){
+			if(in_array($onlinePlayers, $members)){
+				$onlinePlayer->setNameTag(TextFormat::GREEN . $onlinePlayer->getName() . "\n" . TextFormat::YELLOW . $faction);
+			}
+			if(!$mngr->isInFaction($onlinePlayer)){
+				$onlinePlayer->setNameTag(TextFormat::RED . "NA"  . "\n" .  $onlinePlayer->getName());
+			}
+			if(!in_array($onlinePlayer, $members)){
+				$onlinePlayer->setNameTag(TextFormat::RED . $onlinePlayer->getName() . "\n" . TextFormat::YELLOW . $mngr->getPlayerFaction($onlinePlayer->getName()));
+
+			}
+		}
+	}
+
+}
