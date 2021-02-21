1447c700d14e0ebe55d955ed56ba61bb1d031180
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-21 15:02:46 -0500

    Kit Manager Rewrite
    Kit

--- src/vale/hcf/manager/KitsManager.php
+++ src/vale/hcf/manager/KitsManager.php
@@ -2,106 +2,52 @@
 
 namespace vale\hcf\manager;
 
-use jojoe77777\FormAPI\FormAPI;
+use jojoe77777\FormAPI\ModalForm;
 use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\Player;
-use vale\hcf\HCF;
+use pocketmine\Server;
 
 class KitsManager
 {
 
-
-	public HCF $plugin;
-
-	public function __construct(HCF $plugin)
+	public function sendMasterKitForm(Player $player)
 	{
-		$this->plugin = $plugin;
-	}
-
-
-	public function sendKitsMainMenu(Player $player): void
-	{
-		$api = $this->plugin->getServer()->getPluginManager()->getPlugin("FormAPI");
-		$form = $api->createSimpleForm(function (Player $player, int $data = null) {
-			$result = $data;
-			if ($result === null) {
+		$form = new ModalForm(function (Player $player, $data): void {
+			if ($data === null) {
 				return;
 			}
-			switch ($result) {
-
-				case 0:
-					$this->sendMasterKitMenu($player);
+			switch ($data) {
+				case 1:
+					$player->sendMessage("CLAIMED");
 					break;
 
-
+				case 2:
+					$this->sendBaseKitForm($player);
+					break;
 			}
 		});
-		$name = $player->getName();
-		$form->setTitle("§r§8FairyHCF Gkits");
-		$form->addButton("§r§5§lMaster §r§7§lKit" . "\n" . "§r§7Click to Select the kit");
+		$form->setTitle("§6§lFairyHCF §r§7Gkits");
+		$form->setContent("§r§fTEST");
+		$form->setButton1("§r§6§lCLAIM");
+		$form->setButton2("§r§c§lBACK");
 		$form->sendToPlayer($player);
 	}
 
-	####################KITS############################
-
-	public function sendMasterKitMenu(Player $player): void
+	public function sendBaseKitForm(Player $player)
 	{
-		$api = $this->plugin->getServer()->getPluginManager()->getPlugin("FormAPI");
-		$form = $api->createSimpleForm(function (Player $player, int $data = null) {
-			$result = $data;
-			if ($result === null) {
+		$form = new SimpleForm(function (Player $player, $data): void{
+			if($data === null){
 				return;
 			}
-			switch ($result) {
-
+			switch ($data){
 				case 0:
-					if($player->hasPermission("hcf.kits.master.kit"))
-					$player->sendMessage("Claimed");
-					else{
-						$this->noPermforKit($player);
-					}
+					$this->sendMasterKitForm($player);
 					break;
-
-				case 1:
-					$this->sendKitsMainMenu($player);
-					break;
-
-
 			}
 		});
-		$name = $player->getName();
-		$form->setTitle("§r§8Click  'Continue' to use the kit.");
-		$form->setContent("§r§fa fully enchanted §r§5§lMaster §r§7§lKit §r§fthe best kit on the server!" . "\n". "§r§7Purchase kits at §r§a§lstore.hcf.net !");
-		$form->addButton("§r§7Continue");
-		$form->addButton("§r§7Cancel");
-		$form->sendToPlayer($player);
-	}
-
-	public function noPermforKit(Player $player): void
-	{
-		$api = $this->plugin->getServer()->getPluginManager()->getPlugin("FormAPI");
-		$form = $api->createSimpleForm(function (Player $player, int $data = null) {
-			$result = $data;
-			if ($result === null) {
-				return;
-			}
-			switch ($result) {
-
-				case 0:
-                 $this->sendKitsMainMenu($player);
-					break;
-
-				case 1:
-					break;
-
-
-			}
-		});
-		$name = $player->getName();
-		$form->setTitle("§r§8FairyHCF Gkits");
-		$form->setContent("§r§cYou cannnot obtain this kit");
-		$form->addButton("§r§7Back");
-		$form->addButton("§r§7Exit");
+		$form->setTitle("§6§lFairyHCF §r§7Gkits");
+		$form->setContent("§r§6§lSELECT §r§fa Kit!");
+		$form->addButton("§r§4Master §r§7Kit", 0,1,0);
 		$form->sendToPlayer($player);
 	}
 }
