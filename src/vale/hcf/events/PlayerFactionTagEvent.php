8060ce860fe47b53fabe6f750cbfac7277e997d1
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-02-23 22:55:06 -0500

    I FUCKING DID IT GREEN NAMES FOR FACTION MEMBERS

--- src/vale/hcf/events/PlayerFactionTagEvent.php
+++ src/vale/hcf/events/PlayerFactionTagEvent.php
@@ -4,11 +4,14 @@ namespace vale\hcf\events;
 
 use pocketmine\event\Cancellable;
 use pocketmine\event\player\PlayerEvent;
+use pocketmine\network\mcpe\protocol\AddEntityPacket;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use vale\hcf\factions\FactionLoader;
 use vale\hcf\HCF;
+use pocketmine\network\mcpe\protocol\AddActorPacket;
+use pocketmine\entity\Entity;
 
 class PlayerFactionTagEvent extends PlayerEvent implements Cancellable
 {
@@ -40,17 +43,17 @@ class PlayerFactionTagEvent extends PlayerEvent implements Cancellable
 		return $this->player;
 	}
 
-	public function updateFactions()
+	public function updateFactions(Player $player)
 	{
-		$facs = [];
 		$player = $this->getPlayer();
-		$faction = new FactionLoader(HCF::getInstance());
-		$fn = $faction->getPlayerFaction($player->getName());
-		foreach (Server::getInstance()->getOnlinePlayers() as $onlinePlayer) {
-			if (in_array($onlinePlayer->getName(), $faction->getAllMembers($fn))) {
-				$player->setNameTag(TextFormat::GREEN . $player->getName()) . "\n" . TextFormat::YELLOW . $faction->getPlayerFaction($player->getName());
-			} else {
-				$player->setNameTag(TextFormat::RED . $player->getName());
+		$factionmanager = new FactionLoader(HCF::getInstance());
+		$faction = $factionmanager->getPlayerFaction($player->getName());
+		foreach ($factionmanager->getAllMembers($faction) as $member) {
+			$mem = Server::getInstance()->getPlayer($member);
+			if ($mem != null) {
+				$name = $mem->getName();
+				$faction = TextFormat::YELLOW . $factionmanager->getPlayerFaction($mem->getName());
+				$player->sendData($mem, [Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, TextFormat::GREEN . "$name" . "\n" . "$faction"]]);
 			}
 		}
 	}
