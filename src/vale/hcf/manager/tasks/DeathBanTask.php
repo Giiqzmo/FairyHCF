9b3b6866d8e1d43ddc8db18e213987be3ad34a80
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-03-04 19:40:23 -0500

    Update DeathBanTask.php

--- src/vale/hcf/manager/tasks/DeathBanTask.php
+++ src/vale/hcf/manager/tasks/DeathBanTask.php
@@ -4,36 +4,37 @@ namespace vale\hcf\manager\tasks;
 
 use pocketmine\Player;
 use pocketmine\scheduler\Task;
+use pocketmine\Server;
 use vale\hcf\data\YamlProvider;
 use vale\hcf\HCF;
 
 class DeathbanTask extends Task
 {
 
-    /** @var HCF */
-    public HCF $plugin;
-    /** @var Player */
-    public Player $player;
+	/** @var HCF */
+	public HCF $plugin;
+	/** @var Player */
+	public Player $player;
 
-    public function __construct(HCF $plugin)
-    {
-        $this->plugin = $plugin;
-    }
+	public function __construct(HCF $plugin)
+	{
+		$this->plugin = $plugin;
+	}
 
-    public function onRun(int $currentTick)
-    {
-        $deathbanned = HCF::getInstance()->getDeathBannedData()->getAll();
-        foreach ($deathbanned as $p => $time) {
-            if (YamlProvider::$deathBannedPlayers->get($p) <= 1) {
+	public function onRun(int $currentTick)
+	{
+		$deathbanned = HCF::getInstance()->getDeathBannedData()->getAll();
+		foreach ($deathbanned as $p => $time) {
+			if (YamlProvider::$deathBannedPlayers->get($p) <= 1) {
 				YamlProvider::$deathBannedPlayers->remove($p);
 				YamlProvider::$deathBannedPlayers->save();
 
-            } else {
-				YamlProvider::$deathBannedPlayers->set($p, $time - 1);
-				YamlProvider::$deathBannedPlayers->save();
-                //todo lives
-                echo "Revied players ";
-            }
-        }
-    }
-}
+				} else {
+					YamlProvider::$deathBannedPlayers->set($p, $time - 1);
+					YamlProvider::$deathBannedPlayers->save();
+					//todo lives
+				}
+			}
+		}
+	}
+
