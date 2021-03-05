3afae5551c26cb4f779eb9f236024d8dcfba2eb5
vale <60008256+valeeeeyyyyy@users.noreply.github.com>
2021-03-04 19:43:01 -0500

    Update CrateListener.php

--- src/vale/hcf/events/CrateListener.php
+++ src/vale/hcf/events/CrateListener.php
@@ -10,57 +10,15 @@ use vale\hcf\HCF;
 class CrateListener implements Listener
 {
 
-    public HCF $plugin;
+	public HCF $plugin;
 
-    public function __construct(HCF $plugin)
-    {
-        $this->plugin = $plugin;
-        $this->plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
-    }
-/*
-    public function onPlace(BlockPlaceEvent $ev)
-    {
-        $player = $ev->getPlayer();
-        $block = $ev->getBlock();
-        $hand = $player->getInventory()->getItemInHand();
-        $level = (string)$block->getLevel()->getName();
-        $l = $block->getLevel();
-        $names = explode("\n", $hand->getCustomName());
-        $ev->setCancelled();
-        $name = $block->getName();
-        switch ($name) {
-            case "Fairy":
-                if (!$data->exists("FairyCrate")) {
-                    $data->set("FairyCrate", [
-                        "Name" => "FairyCrate",
-                        "PosX" => $block->getX(),
-                        "PosY" => $block->getY(),
-                        "PosZ" => $block->getZ(),
-                        "Level" => $level
-                    ]);
-                    $data->save();
-                } else {
-                    $ev->setCancelled();
-                    $player->sendMessage("Block Already Placed");
-                }
-                break;
-            case "Sylph":
-                if (!$data->exists("SylphCrate")) {
-                    $data->set("SylphCrate", [
-                        "Name" => "SylphCrate",
-                        "PosX" => $block->getX(),
-                        "PosY" => $block->getY(),
-                        "PosZ" => $block->getZ(),
-                        "Level" => $level
-                    ]);
-                    $data->save();
-                } else {
-                    $ev->setCancelled();
-                    $player->sendMessage("Block Already Placed");
-                }
-                break;    
-                //todo add more
-        }
-    }
-*/
+	public function __construct(HCF $plugin)
+	{
+		$this->plugin = $plugin;
+		$this->plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+	}
+
+	public function onBlockPlace(BlockPlaceEvent $event){
+		$player =  $event->getPlayer();
+	}
 }
